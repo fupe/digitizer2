04230bde61e73fdc8b34da47d51333f9ee90d15b
fupe <fupeama@gmail.com>
2025-08-27 16:23:28 +0200

    Guard SimWorker timer from cross-thread start

--- simworker.h
+++ simworker.h
@@ -1,30 +1,30 @@
 // simworker.h
 #pragma once
 #include "idatasourceworker.h"
+#include <QDebug>
 #include <QFile>
 #include <QTimer>
-#include <QDebug>
 
 // Jednoduchý přehrávač z CSV/NDJSON, kde je sloupec ts a data.
 // Pro jednoduchost použijeme CSV: ts_msec;payload
 class SimWorker : public IDataSourceWorker {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    explicit SimWorker(const SimulationParams& params, QObject* parent=nullptr);
+  explicit SimWorker(const SimulationParams &params, QObject *parent = nullptr);
 
 public slots:
-    void open() override;
-    void close() override;
-    void send(const QByteArray&) override {} // simulace neodesílá
+  void open() override;
+  void close() override;
+  void send(const QByteArray &) override {} // simulace neodesílá
 
 private slots:
-    void tick();
+  void tick();
 
 private:
-    SimulationParams params_;
-    QFile file_;
-    qint64 startWallMsec_ = 0;
-    qint64 firstTs_ = -1;
-    QTimer timer_;
-    QString pendingLine_;
+  SimulationParams params_;
+  QFile file_;
+  qint64 startWallMsec_ = 0;
+  qint64 firstTs_ = -1;
+  QTimer *timer_ = nullptr;
+  QString pendingLine_;
 };
